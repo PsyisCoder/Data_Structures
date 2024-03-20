9d77ece7760fba2adcb49b98256722f058336f36
PsyCode <2601793620@qq.com>
2024-03-20 16:24:58 +0800

    两数之和.cpp

--- 两数之和.cpp
+++ 两数之和.cpp
@@ -1,7 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
-
-
-/*题目：给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+/*给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
 
 请你将两个数相加，并以相同形式返回一个表示和的链表。
 
@@ -19,48 +17,45 @@
  * };
  */
 
-/*方法一：模拟
-思路与算法
-
-由于输入的两个链表都是逆序存储数字的位数的，因此两个链表中同一位置的数字可以直接相加。
-
-我们同时遍历两个链表，逐位计算它们的和，并与当前位置的进位值相加。具体而言，如果当前两个链表处相应位置的数字为 n1,n2n1,n2n1,n2，进位值为 carry\textit{carry}carry，则它们的和为 n1+n2+carryn1+n2+\textit{carry}n1+n2+carry；其中，答案链表处相应位置的数字为 (n1+n2+carry)?mod?10(n1+n2+\textit{carry}) \bmod 10(n1+n2+carry)mod10，而新的进位值为 ?n1+n2+carry10?\lfloor\frac{n1+n2+\textit{carry}}{10}\rfloor? 
-10
-n1+n2+carry
-?
- ?。
-
-如果两个链表的长度不同，则可以认为长度短的链表的后面有若干个 000 。
-
-此外，如果链表遍历结束后，有 carry>0\textit{carry} > 0carry>0，还需要在答案链表的后面附加一个节点，节点的值为 carry\textit{carry}carry。
-
-*/
+/*题解：可以依次加链表中对应的两位，把进位记录下来，
+在下一次相加中加上每一次的进位，然后依次保存在新的链表中*/
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* head = nullptr, * tail = nullptr;
-        int carry = 0;
-        while (l1 || l2) {
+        //创建新链表
+        ListNode* head = NULL, * tail = NULL;
+
+        int carry = 0;//表示进位
+
+        //两个链表都遍历完就结束
+        while (l1 || l2)
+        {
             int n1 = l1 ? l1->val : 0;
             int n2 = l2 ? l2->val : 0;
             int sum = n1 + n2 + carry;
-            if (!head) {
+            if (!head)//如果为空链表
+            {
                 head = tail = new ListNode(sum % 10);
+                tail->next = NULL;
             }
-            else {
+            else
+            {
                 tail->next = new ListNode(sum % 10);
                 tail = tail->next;
+                tail->next = NULL;
             }
-            carry = sum / 10;
-            if (l1) {
+            if (l1)
                 l1 = l1->next;
-            }
-            if (l2) {
+            if (l2)
                 l2 = l2->next;
-            }
+            carry = sum / 10;//记录进位
         }
-        if (carry > 0) {
+        //如果出来后还要进位则也要加到链表里
+        if (carry)
+        {
             tail->next = new ListNode(carry);
+            tail = tail->next;
+            tail->next = NULL;
         }
         return head;
     }
