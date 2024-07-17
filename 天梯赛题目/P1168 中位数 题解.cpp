b5aa9db4305d04423fac7bfeb2d649fbcc495705
PsyCode <2601793620@qq.com>
2024-07-17 17:51:44 +0800

    P1168 中位数 题解.cpp

---
+++ 天梯赛题目/P1168 中位数 题解.cpp
@@ -0,0 +1,27 @@
+﻿/*题目：https://www.luogu.com.cn/problem/P1168*/
+
+//题解：每次插入再排序时间消耗太大，用二分法插入（upper_bound或者lower_bound）
+// (前者是返回第一个大于val的地址，后者是返回第一个大于等于val的地址)
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+#define N 100005
+vector<int>A;
+int n;
+int main()
+{
+	cin >> n;
+	for (int i = 1; i <= n; ++i)
+	{
+		int x;
+		cin >> x;
+		A.insert(lower_bound(A.begin(), A.end(), x),x);//二分找出地址再插入
+		if (i % 2 == 1)
+		{
+			cout << A[(A.size() - 1) >> 1] << endl;
+		}
+	}
+}
